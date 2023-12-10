25c32ca429cbfce8aa9b37eed685b03e31892d78
Don Harris <dharris@sudoers.com>
2023-12-10 11:48:31 -0500

    Adding all the necessary files.

---
+++ js/CommonProperties.js
@@ -0,0 +1,28 @@
+// Common Strings
+const makeDonationButton = 'Make a Donation';
+
+// Business information
+const businessName = '';
+const imgLogo = 'img/logo.png';
+const phoneNumber = '(419) 555-1212';
+const email = 'info@YOUR_BUSINESS_NAME.com';
+const address = '123 Main Street <br> Toledo, OH 43615';
+const tagLine = 'ENTER YOUR TAG LINE HERE';
+const missionStatement = 'ENTER YOUR MISSION STATEMENT HERE';
+
+// Footer color for ColorLib to keep the link avaialable
+const colorLibColor = '#fff';
+
+// Social URLs
+const facebookUrl = 'https://www.facebook.com/YOUR_BUSINESS_NAME';
+const pinterestUrl = 'https://www.pinterest.com/YOUR_BUSINESS_NAME';
+const twitterUrl = 'https://twitter.com/YOUR_BUSINESS_NAME';
+const instagramUrl = 'https://www.instagram.com/YOUR_BUSINESS_NAME';
+const linkedInUrl = 'https://www.linkedin.com/company/YOUR_BUSINESS_NAME';
+
+// Social Icon Header Block - add and remove as necessary
+const socialBlock = `<div class="social-icons"><a href="${facebookUrl}" target="_blank"><i class="fa fa-facebook"></i></a>
+                    <a href="${pinterestUrl}" target="_blank"><i class="fa fa-pinterest"></i></a>
+                    <a href="${twitterUrl}" target="_blank"><i class="fa fa-twitter"></i></a>
+                    <a href="${instagramUrl}" target="_blank"><i class="fa fa-instagram"></i></a>
+                    <a href="${linkedInUrl}" target="_blank"><i class="fa fa-linkedin"></i></a></div>`;
